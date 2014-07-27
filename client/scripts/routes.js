52966e3ecd30e277a68e30005cd779339dbd572c
Jeloi <jeremyho19@gmail.com>
2014-07-27 02:22:07 -0700

    add landing template and styles. add ajax mailchimp form submission

--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -1,4 +1,11 @@
+Router.configure({
+    trackPageView: true
+});
+
 Router.map(function() {
   this.route('landing', {path: '/'});
   this.route('palette');
+  this.route('main', {
+  	path: 'main'
+  })
 });
---
+++ server/kadira.js
@@ -0,0 +1,1 @@
+Kadira.connect('DL5NiTiMy4t8rpk43', 'c71810be-2e67-4f58-ab9e-d68169d9660c')
